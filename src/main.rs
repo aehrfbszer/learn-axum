897cb6ba73e798a66655e0bcf224e6b4dffb8796
mizuki <mizukishuiyue8@gmail.com>
2024-10-29 18:08:58 +0800

    init

---
+++ src/main.rs
@@ -0,0 +1,68 @@
+use axum::{
+    http::{
+        header::{AUTHORIZATION, CONTENT_TYPE},
+        StatusCode,
+    },
+    routing::{get, post},
+    Json, Router,
+};
+use serde::{Deserialize, Serialize};
+use tower_http::cors::{Any, CorsLayer};
+use tower_http::services::{ServeDir, ServeFile};
+
+#[tokio::main]
+async fn main() {
+    // build our application with a route
+    let app = Router::new()
+        // `GET /` goes to `root`
+        .route("/hello", get(root))
+        // `POST /users` goes to `create_user`
+        .route("/users", post(create_user).get(root))
+        .nest_service(
+            "/",
+            ServeDir::new("front-end/dist").not_found_service(ServeFile::new("front-end/dist/index.html")),
+        )
+        .layer(
+            CorsLayer::new()
+                .allow_origin(Any)
+                .allow_headers([AUTHORIZATION, CONTENT_TYPE]),
+        );
+
+    // run our app with hyper, listening globally on port 3000
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3001").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+// basic handler that responds with a static string
+async fn root() -> &'static str {
+    "Hello, World!"
+}
+
+async fn create_user(
+    // this argument tells axum to parse the request body
+    // as JSON into a `CreateUser` type
+    Json(payload): Json<CreateUser>,
+) -> (StatusCode, Json<User>) {
+    // insert your application logic here
+    let user = User {
+        id: 1337,
+        username: payload.username,
+    };
+
+    // this will be converted into a JSON response
+    // with a status code of `201 Created`
+    (StatusCode::CREATED, Json(user))
+}
+
+// the input to our `create_user` handler
+#[derive(Deserialize)]
+struct CreateUser {
+    username: String,
+}
+
+// the output to our `create_user` handler
+#[derive(Serialize)]
+struct User {
+    id: u64,
+    username: String,
+}
